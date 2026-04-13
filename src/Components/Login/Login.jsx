252742b3181ceef623f14b914f2b39258e0adec7
Hugo José <hugo.jbcunha@gmail.com>
2026-04-13 13:44:05 -0300

    feat: Adição e correção das logos, correção de design do formulário + icone ocular

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,54 +1,68 @@
+import CliniDeskLogo from './CliniDeskLogo';
 import { FaUser, FaLock } from "react-icons/fa";
+import { Calendar, Shield, MessageCircle } from "lucide-react";
 import "./Login.css";
 
 const Login = () => {
   return (
     <div className="main">
 
-      {/* LADO ESQUERDO */}
       <div className="left">
-        <h1>CliniDesk</h1>
-        <p>Sua recepção, no controle</p>
+        <CliniDeskLogo />
 
         <div className="features">
-          <p>Agendamentos em tempo real</p>
-          <p>Acesso seguro por setor</p>
-          <p>Integração com WhatsApp</p>
+          
+          <div className="feature-item">
+            <div>
+              <Calendar />
+            </div>
+            <span>Agendamentos em tempo real</span>
+          </div>
+
+          <div className="feature-item">
+            <div>
+              <Shield />
+            </div>
+            <span>Acesso seguro por setor</span>
+          </div>
+
+          <div className="feature-item">
+            <div>
+              <MessageCircle />
+            </div>
+            <span>Integração com WhatsApp</span>
+          </div>
+
         </div>
       </div>
 
-      {/* LADO DIREITO */}
       <div className="right">
         <form className="container">
           <h1>Bem-vindo ao CliniDesk</h1>
           <p>Faça login para acessar o painel</p>
 
-          {/* USUÁRIO */}
           <div className="input-box">
+            <FaUser className="left-icon" />
             <input type="text" placeholder="Ex: abcd" />
-            <FaUser className="icon" />
           </div>
 
-          {/* SENHA */}
           <div className="input-box">
+            <FaLock className="left-icon" />
             <input type="password" placeholder="******" />
-            <FaLock className="icon" />
+            <span className="eye">👁</span>
           </div>
 
           <div className="recall-forget">
             <label>
-              <input type="checkbox" />
-              Lembrar de mim
+              <input type="checkbox" /> Lembrar de mim
             </label>
             <a href="#">Esqueci minha senha</a>
           </div>
 
-          <button>Entrar</button>
+          <button type="submit">Entrar</button>
 
           <div className="signup-link">
-            <p>
-              Não tem uma conta? <a href="#">Criar conta</a>
-            </p>
+            <p>Não tem uma conta? <a href="#">Criar conta</a></p>
           </div>
         </form>
       </div>
