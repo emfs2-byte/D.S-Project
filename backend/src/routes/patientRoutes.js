8ffefd0bdfb3f9ce1fbe343be81982af0b9b21c8
vancelmo <vaom@cin.ufpe.br>
2026-04-24 12:29:47 -0300

    feat: implementar segurança nas rotas de cadastro e listagem

--- backend/src/routes/patientRoutes.js
+++ backend/src/routes/patientRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const patientController = require('../controllers/patientController');
+const auth = require('../middlewares/auth');
 
-router.get('/', patientController.getPacientes);
+router.get('/', auth, patientController.getPacientes);
 router.post('/cadastrar', patientController.createPaciente);
 
 module.exports = router;
