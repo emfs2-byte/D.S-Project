86c98b1e7ec7ee6c138a534588a5814e61af9c17
sillyveira <wslc@cin.ufpe.br>
2026-06-26 12:04:54 -0300

    build: mudanças deploy

--- front/src/lib/whatsapp.js
+++
@@ -1,7 +0,0 @@
-export function gerarLinkWhatsApp(telefone, mensagem) {
-    const telefoneLimpo = telefone.replace(/\D/g, '');
-    const link = `https://wa.me/${telefoneLimpo}?text=${encodeURIComponent(mensagem)}`;
-    
-    // O clique do usuário no botão do modal disparará este window.open
-    window.open(link, '_blank');
-}
