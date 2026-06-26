0054708de60195b519c6a0887a0f4b2601bf6e71
Wesley Silveira <wslc@cin.ufpe.br>
2026-06-26 13:45:36 -0300

    build: remove testes

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -1,6 +1,6 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vitest/config"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
@@ -9,9 +9,5 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  test: {
-    environment: "jsdom",
-    globals: true,
   },
 })
